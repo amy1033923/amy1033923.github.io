328b76eb9d904b03d507bd0f14e62fcdfae2b5e3
amy1033923 <amy0911943742@gmail.com>
2022-08-02 10:32:36 +0800

    0802

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,51 @@
-$('.navbar-nav a , .footer-link a').click(function () {
+$('.nav-link , .footer-link a').click(function () {
 	var target = $(this.hash);
 	$('html, body').animate({
 		scrollTop: target.offset().top - $('header').height()
 	}, 500);
 	return false;
+})
+
+$(document).ready(function () {
+	laodImgDone();
 });
+
+function scrollDo() {
+    showAnimation('.addbottom', 'antobottom', 100);
+	showAnimation('.addtop', 'antotop', 100);
+	showAnimation('.addleft', 'antoleft', 100);
+	showAnimation('.addright', 'antoright', 100);
+}
+
+function showAnimation(divName, cssName, offset){
+ 	if ($(divName).length > 0) {
+  		$(divName).each(function() {
+   			var divTop = $(this).offset().top;
+   			var divTopOffset = eval(divTop + offset);
+   			var scrollTop = $(window).scrollTop();
+   			var windowHeight = $(window).height();
+   			if (scrollTop + windowHeight > divTopOffset && scrollTop < divTopOffset) {
+    			$(this).addClass((cssName));
+   			}
+  		});
+ 	}
+}
+
+var maxCount = 100;
+function laodImgDone() {
+ 	maxCount--;
+ 	var allImgTotle = $("img").length;
+ 	var allImgCount = 0;
+ 	$("img").each(function() {
+  		if (this.complete) allImgCount++;
+ 	});
+ 	if(allImgTotle != allImgCount && maxCount > 0) {
+  		setTimeout("laodImgDone()", 200);
+ 	}
+ 	else {
+  		scrollDo();
+  		$(window).scroll(function () {
+   			scrollDo();
+  	});
+  }
+}
